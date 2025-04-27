22261f981cdf5458bec9b2981f6f2ad9ee9a8950
Cryobyte56 <danielasanon54@gmail.com>
2025-04-27 23:44:43 +0800

    Added .env file with variable that make json response message upper case.

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.use('/public', express.static(__dirname + "/public"));
 
@@ -8,7 +9,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message: message });
 });
+
 
 module.exports = app;
