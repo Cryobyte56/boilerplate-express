8d9ce28f77b98c4292ea003c8aad348659d25b4f
Cryobyte56 <danielasanon54@gmail.com>
2025-04-28 07:17:24 +0800

    Added Chained Middleware.

--- myApp.js
+++ myApp.js
@@ -8,23 +8,33 @@ app.use(function middleware(req, res, next) {
   next();
 });
 
-app.use('/public', express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
 app.get("/json", (req, res) => {
   let message = "Hello json";
 
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
-  } else{
-    message = message
+  } else {
+    message = message;
   }
 
   res.json({ message: message });
 });
-
 
 module.exports = app;
