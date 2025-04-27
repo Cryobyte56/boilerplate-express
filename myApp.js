f62b9ea971ef5c2c58c67be26302587b29ea1db5
Cryobyte56 <danielasanon54@gmail.com>
2025-04-27 21:49:14 +0800

    Added json to GET function.

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const app = express();
 app.use('/public', express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.json({"message": "Hello json"});
 });
 
 module.exports = app;
