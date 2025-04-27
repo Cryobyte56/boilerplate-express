78c46a89e8acc43db38d2832be1ca919b33897d2
Cryobyte56 <danielasanon54@gmail.com>
2025-04-27 20:10:25 +0800

    Added public path.

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // Serve static files from 'views' directory
-app.use(express.static(__dirname + "/views"));
+app.use('/public', express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
